8af4e23dc1700b13dee486652b0fe9574de52928
Juan Palazzo <j_palazzo@hotmail.com>
2024-09-02 16:48:15 +0200

    iteracion 1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,47 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
-
-    // 2. shuffleChoices()
+    constructor(text, choices, answer, difficulty){
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        if (difficulty<1){
+            this.difficulty = 1
+        }
+        else if (difficulty>3){
+            this.difficulty = 3
+        }
+        else {
+        this.difficulty = Math.round(difficulty);
+        }
+    }
+    shuffleChoices(){
+        const nuevaLista = []
+        while (this.choices.length>0){
+            console.log(this.choices)
+            nuevaLista.push(this.choices.splice(Math.floor(Math.random()*this.choices.length)[0],1))
+        }
+        this.choices = nuevaLista
+    }
 }
+
+let q1 = new Question ("question", ["choice1","choice2","choice3","choice4"], "ans", 2.3)
+console.log(q1)
+q1.shuffleChoices()
+console.log(q1)
+
+
+
+
+
+
+
+
+
+
+
+// const arrOriginal = [1,2,3,4,5,6,7,8,9,10,]
+// const nuevaLista =[]
+// while (arrOriginal.length>0){
+//     console.log(arrOriginal)
+//     nuevaLista.push(arrOriginal.splice(Math.floor(Math.random()*arrOriginal.length),1))
+// }
+// console.log(nuevaLista)
